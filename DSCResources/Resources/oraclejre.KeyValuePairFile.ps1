660b6467a441a230310ca33ffe90bca41cffe496
Brian Wilhite <bcwilhite@live.com>
2019-08-15 16:52:05 -0400

    dialy commit

--- DSCResources/Resources/oraclejre.KeyValuePairFile.ps1
+++ DSCResources/Resources/oraclejre.KeyValuePairFile.ps1
@@ -5,20 +5,43 @@ $rules = $stig.RuleList | Select-Rule -Type FileContentRule
 
 foreach ($rule in $rules)
 {
-    if ($rule.Key -match "config")
+    switch ($rule.dscresource)
     {
-        $path = $ConfigPath
-    }
-    else
-    {
-        $path = $PropertiesPath
-    }
+        {$_ -eq 'KeyValuePairFile'}
+        {
+            if ($rule.Key -match "config")
+            {
+                $path = $ConfigPath
+            }
+            else
+            {
+                $path = $PropertiesPath
+            }
 
-    KeyValuePairFile "$(Get-ResourceTitle -Rule $rule)"
-    {
-        Path   = $path
-        Name   = $rule.Key
-        Ensure = 'Present'
-        Text   = $rule.Value
+            KeyValuePairFile "$(Get-ResourceTitle -Rule $rule)"
+            {
+                Path   = $path
+                Name   = $rule.Key
+                Ensure = 'Present'
+                Text   = $rule.Value
+            }
+        }
+
+        {$_ -eq 'ReplaceText'}
+        {
+            $content = $rule.Value -split ','
+            foreach ($line in $content)
+            {
+                $line = $line.Trim()
+                ReplaceText "$(Get-ResourceTitle -Rule $rule) $line"
+                {
+                    Path        = $rule.Key
+                    Search      = $line
+                    Type        = 'Text'
+                    Text        = $line
+                    AllowAppend = $true
+                }
+            }
+        }
     }
 }
